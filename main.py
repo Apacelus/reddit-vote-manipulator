bd02f2a5831ff5948013b3962ab69a21e5f8e7c3
Fornball <46828271+Fornball@users.noreply.github.com>
2022-01-18 11:55:18 +0100

    Create general plan for main file
    No real code, mostly just a todo

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+
+launch()
+def launch()
+# check if config exists
+os.path.isfile("config.txt")
+# if not:
+set_config()
+# read config.txt: check stored browser path. Read amount of accounts and passwords and store in
+# available_accounts
+# check if chromedriver is present
+os.path.isfile("chromedriver.exe") 
+# check version match:(https://stackoverflow.com/questions/58979318/is-it-possible-to-check-chromedriver-exe-version-at-runtime-in-python)
+def set_config()
+# open a seperate window(overlaying main window) for config
+# options for 
+# browser select(chromium edge greyed out for now)
+# redownloading chromedriver 
+# opening accounts.txt in default editor 
+def gui()
+# prompt for link and store in comment_link
+# store amount of votes in vote_count
+# two buttons/slider for downvote/upvote, up = true, down = false
+# start button
+# if vote=True in separate thread upvote(vote_count)
+# or vote=False in separate thread downvote(vote_count)
+def upvote(vote_count)
+# start loop with vote_count amount
+while (used_accounts_count < vote_count)
+  # open new incognito and login
+  # navigate to comment_link 
+  # press upvote
+  # close winow
+def downvote(vote_count)
+# start loop with vote_count
+while (used_accounts_count < vote_count)
+  # open new incognito and login
+  # navigate to comment_link 
+  # press downvote
+  # close winow
